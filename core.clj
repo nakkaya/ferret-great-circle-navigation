076fd717104f240bb351056c548e3e98fbd10826
Nurullah Akkaya <nurullah@nakkaya.com>
2017-04-18 19:27:10 +0300

    implement crosstrack-distance

--- core.clj
+++ core.clj
@@ -1,5 +1,9 @@
 (def pi 3.14159265358979323846)
+(def earth-radius 6371.0)
 
+;; calculate the initial bearing (sometimes referred to as forward azimuth) which if
+;; followed in a straight line along a great-circle arc will take you from the start
+;; point to the end point - in degrees
 (defn bearing [c1 c2]
   (let [[lat1 lon1] (map #(to-radians %) c1)
         [lat2 lon2] (map #(to-radians %) c2)]
@@ -13,13 +17,24 @@
                  (cos (- lon2 lon1)))))
           (* 2 pi)))))
 
+;; calculate the great-circle distance between two points - in km
 (defn distance [c1 c2]
   (let [[lat1 lon1] (map #(to-radians %) c1)
         [lat2 lon2] (map #(to-radians %) c2)]
-    (* 2 6371.0
+    (* 2 earth-radius
        (asin
         (sqrt
          (+ (pow (sin (/ (- lat1 lat2) 2)) 2)
             (* (cos lat1)
                (cos lat2)
                (pow (sin (/ (- lon1 lon2) 2)) 2))))))))
+
+;;returns (signed) distance from loc point to great circle defined by start-point and end-point. - in km
+(defn crosstrack-distance [start end loc]
+  (let [dist-start-loc    (distance start loc)
+        bearing-start-loc (bearing start loc)
+        bearing-start-end (bearing start end)]
+
+    (* (asin (* (sin (/ dist-start-loc earth-radius))
+                (sin (to-radians (- bearing-start-loc bearing-start-end)))))
+       earth-radius)))
